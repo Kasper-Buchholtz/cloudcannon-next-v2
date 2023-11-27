b3599ce9217e72eae898c532c571fdac7ccbce3f
Kasper_Buchholtz <kb@superego.nu>
2023-11-27 15:08:12 +0100

    added tailwind and changed the homehero

--- components/layouts/navigation.jsx
+++ components/layouts/navigation.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import data from "../../lib/data";
 import Button from "../button";
+import Image from "next/image";
 export default function Navigation({ children, page }) {
   const [isSticky, setSticky] = useState(false);
 
@@ -23,42 +24,40 @@ export default function Navigation({ children, page }) {
 
   return (
     <>
-      <header class="sticky top-0 right-0 w-full py-4 overflow-hidden se-grid z-top bg-red-500">
-        <div class="col-span-3 col-start-1">
-          <a
-            class=" [&_svg]:w-auto w-auto  block"
-            href="<?php echo se_get_localized_home_url(); ?>"
-            title="<?php wp_title(); ?>"
-          >
-            <img src={data.navigation.logo} alt="Nav-Logo" />
-          </a>
+        <header class="sticky top-0 right-0 w-full py-4 overflow-hidden se-grid z-top bg-red-500">
+            <div class="col-span-3 col-start-1">
+                <Link
+                    class=" [&_svg]:w-auto w-auto  block"
+                    href="/"
+                    title="<?php wp_title(); ?>"
+                >
+                    <Image src={data.navigation.logo} alt="Logo" width={100} height={100} />
+                </Link>
         </div>
         <div class="relative -col-start-1 z-top -col-end-1">
-          <ul className="flex gap-4">
-            {data.navigation.items.map((link, i) => (
-              <li key={i} className="nav-item">
-                <Link
-                  href={`${link.link}`}
-                  className={`nav-link ${
-                    (page?.slug === "index"
-                      ? "home"
-                      : page?.slug?.toLowerCase()) === link.text.toLowerCase()
-                      ? "active"
-                      : ""
-                  }`}
-                >
-                  {link.text}
-                </Link>
-              </li>
-            ))}
+            <ul className="flex gap-4">
+                {data.navigation.items.map((link, i) => (
+                    <li key={i} className="my-auto">
+                        <Link
+                            href={`${link.link}`}
+                            className={`nav-link ${
+                                (page?.slug === "index"
+                                ? "home"
+                                : page?.slug?.toLowerCase()) === link.text.toLowerCase()
+                                ? "active border-b" // Add bg-primary class if the user is on the current page
+                                : ""
+                            }`}
+                        >
+                            {link.text}
+                        </Link>
+                    </li>
+                ))}
+                <li class="ml-4">
+                    <Button variant="primary" href={'/'}>
+                        {data.navigation.button.text}
+                    </Button>
+                </li>
           </ul>
-          <div className="d-none d-lg-block">
-            <div className="nav-item">
-              <Button variant="primary" href={'/'}>
-                {data.navigation.button.text}
-              </Button>
-            </div>
-          </div>
         </div>
       </header>
     </>
