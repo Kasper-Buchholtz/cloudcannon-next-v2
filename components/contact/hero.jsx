529bc6184e18a29bd00ca9ac0bcf53cc582c386d
Kasper_Buchholtz <kb@superego.nu>
2023-11-27 13:56:03 +0100

    Changed tag name

--- components/contact/hero.jsx
+++ components/contact/hero.jsx
@@ -9,8 +9,9 @@ export default function ContactHero({ block, dataBinding }) {
                 <div className="row">
                     <div className="col-lg-6">
                         <div className="contact-hero-content">
-                            <h1>{block.title }</h1>
+                            <h1>{block.title}</h1>
                             <div dangerouslySetInnerHTML={{ __html: md.render(block.description) }}/>
+                            
                         </div>
                     </div>
                 </div>
